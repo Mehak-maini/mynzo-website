91d76ae1ba9adf2ae1128e3fad53f35ed140075c
Mehak Maini <inbox.mehakmaini@gmail.com>
2026-05-05 00:45:10 +0530

    revert css to correct export path

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,5 +1,5 @@
 import { RootLayout } from '@payloadcms/next/layouts'
-import '@payloadcms/next/dist/prod/styles.css'
+import '@payloadcms/next/css'
 import config from '@payload-config'
 import React from 'react'
 import { importMap } from './admin/importMap'
