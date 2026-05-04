37bd994fdedb0bb79356bb7604618c8152de8821
Mehak Maini <inbox.mehakmaini@gmail.com>
2026-05-04 22:53:40 +0530

    Auto-create database tables

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -30,6 +30,7 @@ export default buildConfig({
       url: process.env.DATABASE_URI || `file:${path.resolve(dirname, '../mynzo.db')}`,
       authToken: process.env.DATABASE_AUTH_TOKEN,
     },
+    push: true,
   }),
   sharp,
   plugins: [],
